2c433917a78f93ece02d7720068188c76175fa39
WonHo Bark <wonho.park83@gmail.com>
2016-12-17 21:49:20 +0900

    join 수정

--- app/src/main/java/com/example/j2nn/se_term_project/Join.java
+++ app/src/main/java/com/example/j2nn/se_term_project/Join.java
@@ -1,8 +1,16 @@
 package com.example.j2nn.se_term_project;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by J2NN on 2016-12-17.
  */
 
-public class Join {
+public class Join extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join);
+    }
 }
