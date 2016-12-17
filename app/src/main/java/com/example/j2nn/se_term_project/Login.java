ec56a934ec43432709c5536e7df78990435decb9
WonHo Bark <wonho.park83@gmail.com>
2016-12-17 22:49:42 +0900

    Login 구현중

--- app/src/main/java/com/example/j2nn/se_term_project/Login.java
+++ app/src/main/java/com/example/j2nn/se_term_project/Login.java
@@ -2,6 +2,9 @@ package com.example.j2nn.se_term_project;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by J2NN on 2016-12-17.
@@ -12,5 +15,20 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    }
+
+    public void submit(View view) {
+        final EditText id = (EditText) findViewById(R.id.id);
+        final EditText password = (EditText) findViewById(R.id.password);
+
+        if (true) { //TODO 아이디 DB에서 확인
+            if (false) { //TODO 패스워드 DB에서 확인
+                Toast.makeText(getApplicationContext(), "로그인 되었습니다", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "패스워드가 다릅니다", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "존재하지 않는 아이디입니다.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
